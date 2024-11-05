705d6cf24e1e6149df2f1710cdcfedeea6fc0ccf
wematu <simplyubiquitous2020@gmail.com>
2024-11-05 10:26:23 +0000

    msg: css prefix fix

--- src/app/(protected)/protectedroutetemplate.tsx
+++ src/app/(protected)/protectedroutetemplate.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useAuth } from "../../../hooks/useAuth";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function DashboardPage() {
   const authenticated = useAuth("nerasol");
